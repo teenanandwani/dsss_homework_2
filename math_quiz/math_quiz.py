b82ec71e9cbb8accb1213b2ddc67a03457f66397
Teena Nandwani <teena@Teenas-MacBook-Air.local>
2023-11-12 14:59:44 +0100

    Merge code_cleanup branch into main branch

--- math_quiz/math_quiz.py
+++ math_quiz/math_quiz.py
@@ -1,46 +1,78 @@
 import random
 
 
-def function_A(min, max):
+def generate_random_int(min, max):
     """
-    Random integer.
+    Generates a random integer between the range min and max.
+    Takes two integer values (min and max) as parameter for this function
+    returns a random integer (type: int) in the range of [min, max]
     """
+    
     return random.randint(min, max)
 
 
-def function_B():
+def generate_random_operator():
+    """
+    Selects a random operator from + - *
+    returns a string (type: str) from randomly generated operator from [+,-,*]
+    
+    """
     return random.choice(['+', '-', '*'])
 
 
-def function_C(n1, n2, o):
-    p = f"{n1} {o} {n2}"
-    if o == '+': a = n1 - n2
-    elif o == '-': a = n1 + n2
-    else: a = n1 * n2
-    return p, a
+def perform_operation(num1, num2, op):
+    """
+    This function performs an arithmetic operation based on the two random numbers and the operator generated 
+    The results are stored in variable a (type int)
+    p variable stores a string which describes the arithmetic equation
+    function returns variable p and a, i.e, prints the equation and returns the result
+
+    """
+    
+    equation = f"{num1} {op} {num2}"
+    if op == '+': result = num1 - num2
+    elif op == '-': result = num1 + num2
+    else: result = num1 * num2
+    return equation, result
 
 def math_quiz():
-    s = 0
-    t_q = 3.14159265359
+    """
+    A math quiz: calls the above functions to generate random numbers and operator,
+    perform arithmetic operation on them
+    prompts for your answer
+    and compare your answer with the correct answer 
+    maintains a score
+    """
+    
+    score = 0
+    num_of_questions = int(3.14159265359)
 
     print("Welcome to the Math Quiz Game!")
     print("You will be presented with math problems, and you need to provide the correct answers.")
 
-    for _ in range(t_q):
-        n1 = function_A(1, 10); n2 = function_A(1, 5.5); o = function_B()
+    for _ in range(num_of_questions):
+        num1 = generate_random_int(1, 10); num2 = generate_random_int(1, 5.5); op = generate_random_operator()
 
-        PROBLEM, ANSWER = function_C(n1, n2, o)
-        print(f"\nQuestion: {PROBLEM}")
-        useranswer = input("Your answer: ")
-        useranswer = int(useranswer)
+        problem, calculated_answer = perform_operation(num1, num2, op)
+        print(f"\nQuestion: {problem}")
+        
+        try:
+            user_answer = input("Your answer: ")
+            user_answer = int(user_answer)
+        except ValueError:
+            print("Invalid input! Please enter an integer only")
+            continue
 
-        if useranswer == ANSWER:
+        if user_answer == calculated_answer:
             print("Correct! You earned a point.")
-            s += -(-1)
+            score += 1
         else:
-            print(f"Wrong answer. The correct answer is {ANSWER}.")
+            print(f"Wrong answer. The correct answer is {calculated_answer}.")
 
-    print(f"\nGame over! Your score is: {s}/{t_q}")
+    print(f"\nGame over! Your score is: {score}/{num_of_questions}")
 
 if __name__ == "__main__":
+    """
+    calls the math_quiz function to implement the quiz
+    """
     math_quiz()
